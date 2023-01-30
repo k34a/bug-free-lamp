436c27871d303a1f7ab20fd28e69e604b15d103a
Saksham Garg <saksham2700@gmail.com>
2023-01-30 18:47:47 +0530

    Added more routes

--- front/next.config.js
+++ front/next.config.js
@@ -10,7 +10,16 @@ const nextConfig = {
         pathname: '/uploads/**',
       },
     ], 
-  }
+  },
+  async redirects() {
+    return [
+      {
+        source: '/donate',
+        destination: 'https://www.gofundme.com/f/larry-rowbs-clothing-recycling-initiative',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
