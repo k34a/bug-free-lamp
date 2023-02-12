f2af46cda78a7180a477dd93220bf49294e3f4e1
Saksham Garg <saksham2700@gmail.com>
2023-02-12 11:01:21 +0530

    Removed backend, added notion, all internal links open in new tab

--- back/src/api/join/controllers/join.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * join controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::join.join');
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,6 +3,7 @@ import { Poppins } from '@next/font/google'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 import NextNProgress from 'nextjs-progressbar';
+import Script from 'next/script';
 
 const poppins = Poppins({
   weight: ['400'],
@@ -14,6 +15,7 @@ export default function App({ Component, pageProps }) {
     <>
       <NextNProgress />
       <main className={poppins.className}>
+        <Script src="/static/script.js" />
         <Header />
         <Component {...pageProps} />
         <Footer />
