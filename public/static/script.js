a784a9201aa845f764480347aaa83d2f9507752e
Saksham Garg <saksham2700@gmail.com>
2023-02-13 10:55:26 +0530

    external links in same tab

--- public/static/script.js
+++ public/static/script.js
@@ -1,7 +1,7 @@
 function externalLinks() {
     for (let c = document.getElementsByTagName("a"), a = 0; a < c.length; a++) {
         let b = c[a];
-        b.getAttribute("href") && (b.hostname !== location.hostname && b.hostname !== "larryrowbsfoundation.org") && (b.target = "_blank")
+        b.getAttribute("href") && (b.hostname === location.hostname || b.hostname === "larryrowbsfoundation.org") && (b.target = "_blank")
     }
 };
 
