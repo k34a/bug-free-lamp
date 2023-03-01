b1a0cc46a8aed7660afb8a3d54f58f4a9b86f8b5
Saksham Garg <saksham2700@gmail.com>
2023-03-01 14:25:50 +0530

    added description

--- src/pages/webinar.js
+++ src/pages/webinar.js
@@ -6,7 +6,10 @@ export default function Webinar(params) {
     return (
         <>
             <Head>
-                <title>Webinar</title>
+                <title>Stylish Sustainability Webinar - Larry Rowbs Foundation</title>
+                <meta name="description" content="Join the Stylish Sustainability Webinar to explore how taking a step towards sustainable fashion may brighten the future of Africa, and how Larry Rowbs Foundation is working on its forefront. Register Now." />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/favicon.ico" />
             </Head>
             <div className={styles.mainbody}>
                 <RegistrationForm />
