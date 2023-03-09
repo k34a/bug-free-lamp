c5092a5a205bc2dcf310b2ab54038654f9d464d5
Saksham Garg <saksham2700@gmail.com>
2023-03-09 18:50:50 +0530

    corrected className

--- src/components/Youtube.js
+++ src/components/Youtube.js
@@ -1,6 +1,6 @@
 const YoutubeVideo = (props) => {
     return (
-        <div class="aspect-w-16 aspect-h-9">
+        <div className="aspect-w-16 aspect-h-9">
             <iframe src="https://www.youtube.com/embed/sNYn-M6TfII" title="Larry Rowbs Foundation Mission" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
         </div>
     );
