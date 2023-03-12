685b924598da8148d22693e7c71139618022d827
Saksham Garg <saksham2700@gmail.com>
2023-03-12 19:11:01 +0530

    changed tabs to div

--- src/components/homepage/Benefits.js
+++ src/components/homepage/Benefits.js
@@ -19,7 +19,7 @@ const tabsConfig = [
                 <br /><br /> Together, we can create a cleaner, healthier, and greener planet through sustainable fashion.
             </p>),
             head: "Developing a Circular Fashion Economy",
-            bg: "bg-gradient-to-r from-teal-200 to-teal-500",
+            bg: "bg-gradient-to-r from-green-200 to-green-500",
             buttonBg: "bg-green-700",
             buttonBgHover: "bg-green-800",
         }
@@ -48,6 +48,27 @@ const tabsConfig = [
         }
     ],
     [
+        "Enhanced Wellness",
+        "red",
+        "fa fa-heartbeat",
+        "border-red-600",
+        {
+            img: "./Safeguarding-health-with-sustainable-fashion.svg",
+            alt: "Safeguarding health by Sustainable Fashion",
+            content: (<p>
+                Fast fashion not only harms the environment, but also poses a threat to our health. The use of cheap-toxic chemicals in the production process can cause skin irritation, allergies, and even lead to cancer.
+                <br /><br />
+                At Larry Rowbs Foundation, we are dedicated to creating a healthier future for all by producing sustainable and safe clothing. Our clothes are made from non-toxic materials and undergo strict quality control to ensure their safety for you and the environment. By choosing sustainable fashion, we are not only helping the planet, but also taking care of our own health and well-being.
+                <br /><br />
+                Join us in this mission to create a safer, greener, and healthier world for all.
+            </p>),
+            head: "Safeguarding Health",
+            bg: "bg-gradient-to-r from-amber-400 to-yellow-500",
+            buttonBg: "bg-red-700",
+            buttonBgHover: "bg-red-800",
+        }
+    ],
+    [
         "Fashion Education", 
         "pink", 
         "fa fa-graduation-cap", 
@@ -70,75 +91,39 @@ const tabsConfig = [
             buttonBgHover: "bg-violet-700",
         }
     ],
-    [
-        "Enhanced Wellness", 
-        "red", 
-        "fa fa-heartbeat", 
-        "border-red-600",
-        {
-            img: "./Safeguarding-health-with-sustainable-fashion.svg",
-            alt: "Safeguarding health by Sustainable Fashion",
-            content: (<p>
-                Fast fashion not only harms the environment, but also poses a threat to our health. The use of cheap-toxic chemicals in the production process can cause skin irritation, allergies, and even lead to cancer.
-                <br /><br />
-                At Larry Rowbs Foundation, we are dedicated to creating a healthier future for all by producing sustainable and safe clothing. Our clothes are made from non-toxic materials and undergo strict quality control to ensure their safety for you and the environment. By choosing sustainable fashion, we are not only helping the planet, but also taking care of our own health and well-being.
-                <br /><br />
-                Join us in this mission to create a safer, greener, and healthier world for all.
-            </p>),
-            head: "Safeguarding Health",
-            bg: "bg-gradient-to-r from-amber-400 to-yellow-500",
-            buttonBg: "bg-red-700",
-            buttonBgHover: "bg-red-800",
-        }
-    ]
 ]
 
 export default function Benefits() {
-    const [openTab, setOpenTab] = useState(2);
-    const tabs = tabsConfig.map((ele, index)=>{
-        return (
-            <li key={index}>
-                <button
-                    className={
-                        `text-xs font-bold uppercase px-5 py-3 shadow-lg border-2 rounded block leading-normal no-underline ${openTab === index + 1 && `border-b-4 ${ele[3]}`}`
-                    }
-                    onClick={e => {
-                        e.preventDefault();
-                        setOpenTab(index+1);
-                    }}
-                    data-toggle="tab"
-                    role="tablist"
-                >
-                    <i className={`${ele[2]} text-base mr-1`}></i> {ele[0]}
-                </button>
-            </li>
-        )
-    })
-
     const tabsContent = tabsConfig.map((ele, index) => {
         return (
-            <div key={index} className="md:text-lg lg:text-xl text-white">
-                <div className={openTab === index+1 ? "block" : "hidden"}>
-                    <img
-                        src={ele[4]["img"]}
-                        alt={ele[4]["alt"]}
-                        className="w-1/4 m-auto"
-                    />
-                    <h2 className="text-2xl my-4 md:text-3xl lg:text-4xl font-black">{ele[4]["head"]}</h2>
-                    {ele[4]["content"]}
-                    <br /><br />
-                    <Link href="/blog">
-                        <button 
-                            type="button"
-                            className={`text-white ${ele[4]["buttonBg"]} hover:${ele[4]["buttonBgHover"]} focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center no-underline`}
-                        >
-                            Learn More
-                            <svg aria-hidden="true" className="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                <path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd">
-                                </path>
-                            </svg>
-                        </button>
-                    </Link>
+            <div key={index} className={`w-full min-w-0 break-words py-24 rounded ${ele[4]["bg"]}`}>
+                <div className="px-4 py-5 flex-auto">
+                    <div className="tab-content tab-space w-4/5 md:w-4/6 lg:w-1/2 m-auto">
+                        <div className={`md:text-lg lg:text-xl text-white`}>
+                            <div>
+                                <img
+                                    src={ele[4]["img"]}
+                                    alt={ele[4]["alt"]}
+                                    className="w-1/2 sm:w-1/3 lg:w-1/4 m-auto"
+                                />
+                                <h2 className="text-2xl my-4 md:text-3xl lg:text-4xl font-black">{ele[4]["head"]}</h2>
+                                {ele[4]["content"]}
+                                <br /><br />
+                                <Link href="/blog">
+                                    <button 
+                                        type="button"
+                                        className={`text-white ${ele[4]["buttonBg"]} hover:${ele[4]["buttonBgHover"]} focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center no-underline`}
+                                    >
+                                        Learn More
+                                        <svg aria-hidden="true" className="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                            <path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd">
+                                            </path>
+                                        </svg>
+                                    </button>
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         );
@@ -147,24 +132,8 @@ export default function Benefits() {
         <>
             <div className="flex flex-wrap">
                 <div className="w-full relative">
-                    <div className="text-center">
-                        <div className="inline-block">
-                            <ul
-                                className="flex mb-0 list-none flex-wrap flex-row"
-                                role="tablist"
-                            >
-                                {tabs}
-                            </ul>
-                        </div>
-                    </div>
-                    <div className={`relative flex flex-col min-w-0 break-words ${tabsConfig[openTab-1][4]["bg"]} w-full py-24 rounded`}>
-                        <div className="px-4 py-5 flex-auto">
-                            <div className="tab-content tab-space w-4/5 md:w-4/6 lg:w-1/2 m-auto">
-                                {tabsContent}
-                            </div>
-                        </div>
-                    </div>
-                    <div className={styles.customShapeDivider}>
+                    {tabsContent}
+                    {/*<div className={styles.customShapeDivider}>
                         <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
                             <path 
                                 d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" 
@@ -172,7 +141,7 @@ export default function Benefits() {
                             >
                             </path>
                         </svg>
-                    </div>
+                    </div>*/}
                 </div>
             </div>
         </>
