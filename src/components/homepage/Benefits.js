9de5bbf21be13e9afd61392012c30ed258dbd006
Saksham Garg <saksham2700@gmail>
2023-03-24 08:19:41 +0530

    seo booster

--- src/components/homepage/Benefits.js
+++ src/components/homepage/Benefits.js
@@ -3,122 +3,99 @@ import React, { useState } from "react";
 import styles from "@/styles/Benefits.module.css"
 
 const tabsConfig = [
-    [
-        "Greener Environment", 
-        "green", 
-        "fa fa-leaf", 
-        "border-green-600",
-        {
-            img: "./Green-environment-by-recycling.svg",
-            alt: "Green environment by Sustainable Fashion",
-            content: (<p>
-                We are promoting a cleaner and greener planet through sustainable fashion. We do this by taking action on two fronts. First, we actively work to recycle clothing from landfills, reducing the amount of waste that ends up in the environment. This not only helps to preserve our planet&#39;s resources but also supports our mission to create a more sustainable fashion industry.
-                <br />
-                <br />
-                Second, we rely on the generosity of people like you to help us achieve our goals. By donating your gently used clothing to us, you play an active role in supporting our mission. Whether you are donating a single item or a whole wardrobe, your contributions help us to keep clothing out of landfills and promote a more sustainable future for the fashion industry.
-                <br /><br /> Together, we can create a cleaner, healthier, and greener planet through sustainable fashion.
-            </p>),
-            head: "Developing a Circular Fashion Economy",
-            bg: "bg-gradient-to-r from-green-200 to-green-500",
-            buttonBg: "bg-green-700",
-            buttonBgHover: "bg-green-800",
-        }
-    ],
-    [
-        "Boosted Economy", 
-        "sky", 
-        "fa fa-money", 
-        "border-sky-600",
-        {
-            img: "./boosting-economy-through-sustainable-fashion.svg",
-            alt: "Boosting Economy with Sustainable Fashion",
-            content: (<p>
-                We believe in the power of sustainable fashion to drive economic growth, especially in underdeveloped areas like those in Africa.
-                <br />
-                <br />
-                By creating biodegradable and recycled clothes, we are cleaning up landfills, creating more job opportunities, and providing better wages (and working conditions) to the workers in the fashion industry.
-                <br />
-                <br />
-                We understand the importance of investing in local communities and helping to boost the economy while also promoting sustainability and ethical practices. By doing so, we are not only creating a better future for the environment but also for the individuals and communities we aim to serve.
-            </p>),
-            head: "Creating More Jobs",
-            bg: "bg-gradient-to-r from-indigo-500 to-blue-500",
-            buttonBg: "bg-sky-700",
-            buttonBgHover: "bg-sky-800",
-        }
-    ],
-    [
-        "Enhanced Wellness",
-        "red",
-        "fa fa-heartbeat",
-        "border-red-600",
-        {
-            img: "./Safeguarding-health-with-sustainable-fashion.svg",
-            alt: "Safeguarding health by Sustainable Fashion",
-            content: (<p>
-                Fast fashion not only harms the environment, but also poses a threat to our health. The use of cheap-toxic chemicals in the production process can cause skin irritation, allergies, and even lead to cancer.
-                <br /><br />
-                At Larry Rowbs Foundation, we are dedicated to creating a healthier future for all by producing sustainable and safe clothing. Our clothes are made from non-toxic materials and undergo strict quality control to ensure their safety for you and the environment. By choosing sustainable fashion, we are not only helping the planet, but also taking care of our own health and well-being.
-                <br /><br />
-                Join us in this mission to create a safer, greener, and healthier world for all.
-            </p>),
-            head: "Safeguarding Health",
-            bg: "bg-gradient-to-r from-amber-400 to-yellow-500",
-            buttonBg: "bg-red-700",
-            buttonBgHover: "bg-red-800",
-        }
-    ],
-    [
-        "Fashion Education", 
-        "pink", 
-        "fa fa-graduation-cap", 
-        "border-pink-600",
-        {
-            img: "./Charitable-Fashion-school.svg",
-            alt: "Charitable Fashion School",
-            content: (<p>
-                Do you know that it costs thousands of dollars to study from a fashion-school?.
-                <br /><br />
-                <b>How we are helping in that?</b>
-                <br /><br />
-                The revenue generated from selling sustainable fashion products will be re-invested in establishing and running a <u>Charitable Fashion School</u>. The students who are passionate about driving change with sustainable fashion will learn all of it for free!
-                <br /><br />
-                Our goal is to provide education and training in sustainable fashion practices to individuals from all walks of life, free of charge. By educating the next generation of fashion professionals and designers, we hope to foster a more responsible and sustainable fashion industry. Our aim is to build a complete ecosystem for talented individuals, and raise awareness about the importance of ethical and sustainable practices in the fashion industry. By doing so, we believe that we can make a positive impact on the fashion industry and help to create a more equitable and sustainable world.
-            </p>),
-            head: "Free-of-cost Fashion School",
-            bg: "bg-gradient-to-r from-violet-500 to-purple-500",
-            buttonBg: "bg-violet-800",
-            buttonBgHover: "bg-violet-700",
-        }
-    ],
+    {
+        img: "./Green-environment-by-recycling.svg",
+        alt: "Green environment by Sustainable Fashion",
+        content: (<p>
+            We are promoting a cleaner and greener planet through sustainable fashion. We do this by taking action on two fronts. First, we actively work to recycle clothing from landfills, reducing the amount of waste that ends up in the environment. This not only helps to preserve our planet&#39;s resources but also supports our mission to create a more sustainable fashion industry.
+            <br />
+            <br />
+            Second, we rely on the generosity of people like you to help us achieve our goals. By donating your gently used clothing to us, you play an active role in supporting our mission. Whether you are donating a single item or a whole wardrobe, your contributions help us to keep clothing out of landfills and promote a more sustainable future for the fashion industry.
+            <br /><br /> Together, we can create a cleaner, healthier, and greener planet through sustainable fashion.
+        </p>),
+        head: "Developing a Circular Fashion Economy",
+        bg: "bg-gradient-to-r from-green-200 to-green-500",
+        buttonBg: "bg-green-700",
+        buttonBgHover: "bg-green-800",
+        buttonText: "Conscious fashion rocks!",
+    },
+    {
+        img: "./boosting-economy-through-sustainable-fashion.svg",
+        alt: "Boosting Economy with Sustainable Fashion",
+        content: (<p>
+            We believe in the power of sustainable fashion to drive economic growth, especially in underdeveloped areas like those in Africa.
+            <br />
+            <br />
+            By creating biodegradable and recycled clothes, we are cleaning up landfills, creating more job opportunities, and providing better wages (and working conditions) to the workers in the fashion industry.
+            <br />
+            <br />
+            We understand the importance of investing in local communities and helping to boost the economy while also promoting sustainability and ethical practices. By doing so, we are not only creating a better future for the environment but also for the individuals and communities we aim to serve.
+        </p>),
+        head: "Creating More Jobs",
+        bg: "bg-gradient-to-r from-indigo-500 to-blue-500",
+        buttonBg: "bg-blue-600",
+        buttonBgHover: "bg-blue-700",
+        buttonText: "Wear your values!",
+    },
+    {
+        img: "./Safeguarding-health-with-sustainable-fashion.svg",
+        alt: "Safeguarding health by Sustainable Fashion",
+        content: (<p>
+            Fast fashion not only harms the environment, but also poses a threat to our health. The use of cheap-toxic chemicals in the production process can cause skin irritation, allergies, and even lead to cancer.
+            <br /><br />
+            At Larry Rowbs Foundation, we are dedicated to creating a healthier future for all by producing sustainable and safe clothing. Our clothes are made from non-toxic materials and undergo strict quality control to ensure their safety for you and the environment. By choosing sustainable fashion, we are not only helping the planet, but also taking care of our own health and well-being.
+            <br /><br />
+            Join us in this mission to create a safer, greener, and healthier world for all.
+        </p>),
+        head: "Safeguarding Health",
+        bg: "bg-gradient-to-r from-amber-400 to-yellow-500",
+        buttonBg: "bg-red-700",
+        buttonBgHover: "bg-red-800",
+        buttonText: "Fast fashion? No thanks!",
+    },
+    {
+        img: "./Charitable-Fashion-school.svg",
+        alt: "Charitable Fashion School",
+        content: (<p>
+            Do you know that it costs thousands of dollars to study from a fashion-school?.
+            <br /><br />
+            <b>How we are helping in that?</b>
+            <br /><br />
+            The revenue generated from selling sustainable fashion products will be re-invested in establishing and running a <u>Charitable Fashion School</u>. The students who are passionate about driving change with sustainable fashion will learn all of it for free!
+            <br /><br />
+            Our goal is to provide education and training in sustainable fashion practices to individuals from all walks of life, free of charge. By educating the next generation of fashion professionals and designers, we hope to foster a more responsible and sustainable fashion industry. Our aim is to build a complete ecosystem for talented individuals, and raise awareness about the importance of ethical and sustainable practices in the fashion industry. By doing so, we believe that we can make a positive impact on the fashion industry and help to create a more equitable and sustainable world.
+        </p>),
+        head: "Free-of-cost Fashion School",
+        bg: "bg-gradient-to-r from-violet-500 to-purple-500",
+        buttonBg: "bg-violet-800",
+        buttonBgHover: "bg-violet-700",
+        buttonText: "Fashion with a purpose!",
+    },
 ]
 
 export default function Benefits() {
     const tabsContent = tabsConfig.map((ele, index) => {
         return (
-            <div key={index} className={`w-full min-w-0 break-words py-24 rounded ${ele[4]["bg"]}`}>
+            <div key={index} className={`w-full min-w-0 break-words py-24 rounded ${ele["bg"]}`}>
                 <div className="px-4 py-5 flex-auto">
                     <div className="tab-content tab-space w-4/5 md:w-4/6 lg:w-1/2 m-auto">
                         <div className={`md:text-lg lg:text-xl text-white`}>
                             <div>
                                 <img
-                                    src={ele[4]["img"]}
-                                    alt={ele[4]["alt"]}
+                                    src={ele["img"]}
+                                    alt={ele["alt"]}
+                                    loading="lazy"
                                     className="w-1/2 sm:w-1/3 lg:w-1/4 m-auto"
                                 />
-                                <h2 className="text-2xl my-4 md:text-3xl lg:text-4xl font-black">{ele[4]["head"]}</h2>
-                                {ele[4]["content"]}
+                                <h2 className="text-2xl my-4 md:text-3xl lg:text-4xl font-black">{ele["head"]}</h2>
+                                {ele["content"]}
                                 <br /><br />
-                                <Link href="/blog">
+                                <Link href="/blog" className="!no-underline">
                                     <button 
                                         type="button"
-                                        className={`text-white ${ele[4]["buttonBg"]} hover:${ele[4]["buttonBgHover"]} focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center no-underline`}
+                                        className={`text-white ${ele["buttonBg"]} hover:${ele["buttonBgHover"]} focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-3 text-center inline-flex items-center`}
                                     >
-                                        Learn More
-                                        <svg aria-hidden="true" className="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                            <path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd">
-                                            </path>
-                                        </svg>
+                                        {ele["buttonText"]}
                                     </button>
                                 </Link>
                             </div>
