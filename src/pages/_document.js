650a101358186762abc165876c6ca1b9458cb836
Saksham Garg <saksham2700@gmail>
2023-04-03 17:36:06 +0530

    removing colors in mobile

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,9 +5,6 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta name="google-site-verification" content="KyGlTO_S7Lg70l1jUDsUImlqZvPCdStcxP44VZwMlSE" />
-        <meta name="theme-color" content="#b700ff" />
-        <meta name="msapplication-navbutton-color" content="#b700ff" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="#b700ff" />
       </Head>
       <body>
         <Main />
