d077a1c926f3501eb0241510386f3e94d138fd1b
Saksham Garg <saksham2700@gmail>
2023-04-22 13:51:08 +0530

    added changemaker

--- src/pages/donate.js
+++ src/pages/donate.js
@@ -10,27 +10,25 @@ export default function Donate() {
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <main>
-                <div className='w-11/12 md:w-2/3 m-auto'>
-                    <h1 className='text-4xl font-black text-center m-12 opacity-70 text-rose-600'>Be a Sustainable Fashion Hero: Donate Today!</h1>
+            <main className="py-6 md:py-12 word-break">
+                <div className='w-11/12 md:w-2/3 m-auto prose md:prose-lg'>
+                    <h1 className='text-3xl md:text-4xl font-black md:text-center m-12 opacity-70 text-rose-600'>Support us and be a Sustainable Fashion Hero!</h1>
                     <p>
-                        When you donate to Larry Rowbs Foundation, you help us fight the hazards of fast fashion, improve the well-being of workers in the social industry, and create a sustainable future. Your contribution, no matter the size, can make a meaningful impact and support our mission.
+                        When you donate to Larry Rowbs Foundation, you help us fight the hazards of fast fashion, improve the well-being of workers in the social industry, and create a sustainable future. Your contribution, no matter the size, can make a <b>meaningful impact</b> and support our mission.
                     </p>
-                    <br />
                     <p>
-                        <u><b>Benefits of Donation:</b></u>
+                        <u><b>What are the benefits of your donation?</b></u>
                     </p>
-                    <ul className='list-disc list-inside'>
-                        <li>Promote Sustainable Fashion: Your donation supports initiatives that raise awareness about sustainable fashion and its benefits for the environment and society.</li>
-                        <li>Support Workers&apos; Rights: Your donation helps us improve the well-being of workers in the social industry and promote fair labor practices.</li>
-                        <li>Create a Sustainable Future: Your donation funds research and development of sustainable materials and technologies that can reduce the environmental impact of the fashion industry.</li>
-                        <li>Make a Difference: Your donation can create a positive change and support our efforts to create a more ethical and eco-friendly fashion industry.</li>
-                    </ul>
+                    <ol>
+                        <li><span className="font-bold text-sky-500">Promote Sustainable Fashion</span>: Your donation supports initiatives that raise awareness about sustainable fashion and its benefits for the environment and society.</li>
+                        <li><span className="font-bold text-pink-500">Support Workers&apos; Rights</span>: Your donation helps us improve the well-being of workers in the social industry and promote fair labor practices.</li>
+                        <li><span className="font-bold text-green-500">Create a Sustainable Future</span>: Your donation funds research and development of sustainable materials and technologies that can reduce the environmental impact of the fashion industry.</li>
+                        <li><span className="font-bold text-orange-500">Make a Difference</span>: Your donation can create a positive change and support our efforts to create a more ethical and eco-friendly fashion industry.</li>
+                    </ol>
                     <p>
-                        <br />
                         Whether you make a one-time donation or become a monthly donor, your contribution can make a significant difference. Join us in our mission to promote sustainable fashion and create a more equitable and sustainable future for all.
-                        <br />
-                        <br />
+                    </p>
+                    <p>
                         Thank you for your support!
                     </p>
                 </div>
