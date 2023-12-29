b9759ea57614c229e6181528e7a8440c1470c716
Saksham Garg <saksham2700@gmail>
2023-12-29 18:40:25 +0530

    re-added HSTS header

--- next.config.js
+++ next.config.js
@@ -6,6 +6,10 @@ const securityHeaders = [
         value: 'on'
     },
     {
+        key: 'Strict-Transport-Security',
+        value: 'max-age=63072000; includeSubDomains; preload'
+    },
+    {
         key: 'X-XSS-Protection',
         value: '1; mode=block'
     },
