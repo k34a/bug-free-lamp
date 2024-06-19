4900a120fb1401459f77b149477e6ee9900826db
Saksham Garg <saksham2700@gmail.com>
2024-06-19 18:46:30 +0530

    added proper email handling

--- src/lib/emailfns.js
+++ src/lib/emailfns.js
@@ -1,30 +1,34 @@
 export async function emailNotifier(emailID, emailSubject, emailBody) {
     const fromEmailId = process.env.FROM_EMAIL_ID;
     const fromEmailPassword = process.env.FROM_EMAIL_PASSWORD;
-    let nodemailer = require('nodemailer')
-    const transporter = nodemailer.createTransport({
-        host: 'smtp.zoho.in',
-        secure: true,
-        port: 465,
-        auth: {
-            user: fromEmailId,
-            pass: fromEmailPassword,
-        },
-    });
-    const mailData = {
-        from: fromEmailId,
-        to: emailID,
-        subject: emailSubject,
-        html: emailBody
-    }
-    console.info(`Trying to send email to ${emailID} with subject: ${emailSubject}.`)
-    transporter.sendMail(mailData, function (err, info) {
-        if (err) {
-            throw err;
-        } else {
-            console.info(info);
-            transporter.close();
-            return true;
+    let nodemailer = require('nodemailer');
+
+    return new Promise((resolve, reject) => {
+        const transporter = nodemailer.createTransport({
+            host: 'smtp.zoho.in',
+            secure: true,
+            port: 465,
+            auth: {
+                user: fromEmailId,
+                pass: fromEmailPassword,
+            },
+        });
+        const mailData = {
+            from: fromEmailId,
+            to: emailID,
+            subject: emailSubject,
+            html: emailBody
         }
-    })
+        console.info(`Trying to send email to ${emailID} with subject: ${emailSubject}.`)
+        transporter.sendMail(mailData, function (err, info) {
+            if (err) {
+                console.error(err);
+                reject(false);
+            } else {
+                console.info(info);
+                transporter.close();
+                resolve(true);
+            }
+        })
+    });
 }
