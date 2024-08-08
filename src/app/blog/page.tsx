a19e6b19db1c52a96e9c707343b83142b8e264dd
Saksham Garg <saksham2700@gmail.com>
2024-08-08 19:09:27 +0530

    minor change

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -2,6 +2,31 @@ import CardList from "@/components/blog/card-list";
 import Pagination from "@/components/blog/filtering/pagination";
 import { getBlogAriclesList } from "@/content/fns";
 
+export async function generateMetadata() {
+    const desc =
+        "The Larry Rowbs Foundation Blog. Dive into the latest news, updates, and educational content on making fashion sustainable.";
+    const title = "Blog | Larry Rowbs Foundation";
+    return {
+        title: title,
+        twitter: {
+            title: title,
+            description: desc,
+            site: "larryrowbsfoundation.org",
+        },
+        openGraph: {
+            title: title,
+            description: desc,
+            locale: "en_US",
+            site_name: "Larry Rowbs Foundation",
+            type: "article",
+        },
+        description: desc,
+        keywords: "sustainable-fashion, africa",
+        author: "Larry Rowbs Foundation",
+        robots: "index, follow",
+    };
+}
+
 const Page = () => {
     const { articles, totalPages } = getBlogAriclesList("", [], 1);
 
